927ae2b9897c49d15133343d7d828801b462c3f6
David Whitney <david@davidwhitney.co.uk>
2018-12-05 11:42:20 +0000

    rename

--- WhereIs/Locate.cs
+++ WhereIs/Locate.cs
@@ -10,9 +10,9 @@ using Newtonsoft.Json;
 
 namespace WhereIs
 {
-    public static class Function1
+    public static class Locate
     {
-        [FunctionName("Function1")]
+        [FunctionName("Locate")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
