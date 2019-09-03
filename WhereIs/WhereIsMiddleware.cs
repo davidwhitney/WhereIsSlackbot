271475e3f1f5d6a2bc09e7603e8d041faea38e6a
David Whitney <david@davidwhitney.co.uk>
2019-09-03 22:01:16 +0100

    Necromancy, stripping this down to push to GCP.

---
+++ WhereIs/WhereIsMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using WhereIs.Slack;
+
+namespace WhereIs
+{
+    public class WhereIsMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            var requestBody = await new StreamReader(context.Request.Body).ReadToEndAsync();
+            var request = PayloadMapper.Map(requestBody);
+
+            var slackResponse = new SlackResponse
+            {
+                text = "I know where that is!",
+                attachments =
+                {
+                    text = "Describing the image right here...",
+                    image_url = "https://a.slack-edge.com/ae57/img/slack_api_logo.png"
+                }
+            };
+
+
+            var greeting = "Hello";
+            await context.Response.WriteAsync(greeting);
+        }
+    }
+}
