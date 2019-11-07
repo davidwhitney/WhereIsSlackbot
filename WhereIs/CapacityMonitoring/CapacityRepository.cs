1b1882ab3828ef60e223a94a79e73fe8fb94b01a
David Whitney <david@davidwhitney.co.uk>
2019-11-07 20:28:02 +0000

    Tighter use of storage.

--- WhereIs/CapacityMonitoring/CapacityRepository.cs
+++ WhereIs/CapacityMonitoring/CapacityRepository.cs
@@ -20,37 +20,27 @@ namespace WhereIs.CapacityMonitoring
         public Dictionary<string, int> Load()
         {
             var blobClient = _storageClient.GetBlobClient("dev.json");
-
-            while (true)
+            var blob = blobClient.Download(CancellationToken.None);
+            using (var memoryStream = new MemoryStream())
             {
-                try
-                {
-                    var blob = blobClient.Download(CancellationToken.None);
-                    using (var memoryStream = new MemoryStream())
-                    {
-                        blob.Value.Content.CopyToAsync(memoryStream);
-                        var text = Encoding.UTF8.GetString(memoryStream.ToArray());
-                        return JsonConvert.DeserializeObject<Dictionary<string, int>>(text);
-                    }
-                }
-                catch
-                {
-                    // Lol, fuck the world, fake acid compliance.
-                }
+                blob.Value.Content.CopyToAsync(memoryStream);
+                var text = Encoding.UTF8.GetString(memoryStream.ToArray());
+                return JsonConvert.DeserializeObject<Dictionary<string, int>>(text);
             }
         }
 
         public void Save(Dictionary<string, int> state)
         {
+            var blobClient = _storageClient.GetBlobClient("dev.json");
             var asString = JsonConvert.SerializeObject(state);
+
             using (var memoryStream = new MemoryStream())
             {
                 var bytes = Encoding.UTF8.GetBytes(asString);
                 memoryStream.Write(bytes);
                 memoryStream.Position = 0;
 
-                var deleteResponse = _storageClient.DeleteBlobIfExists("dev.json");
-                var response = _storageClient.UploadBlob("dev.json", memoryStream, CancellationToken.None);
+                blobClient.Upload(memoryStream, true, CancellationToken.None);
             }
         }
     }
